c942d5bbdede3307f1024adc222f2cea5ce231a7
hyein0112 <bhi12134@gmail.com>
2021-11-08 20:44:54 +0900

    Controller 폴더

--- GodOfDiet/GodOfDiet/Controller/MainViewController.swift
+++ GodOfDiet/GodOfDiet/Controller/MainViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 import SnapKit
 import Then
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
